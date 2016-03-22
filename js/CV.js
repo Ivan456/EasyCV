6373098b3b9ee92f6fff8a848f8176e07115a6da
ivan456 <ivan.vodich.l@gmail.com>
2016-03-22 09:36:08 +0300

    set my colors in css

--- js/CV.js
+++ js/CV.js
@@ -1,3 +1,11 @@
 function CV() {
-	
+	this.name = "name";
+	this.profession = "profession";
+	this.generalInformation = "generalInformation: age, home, telephone, email";
+	this.education = "education: colegue, university";
+	this.wantedProfession = "  wanted profession";
+	this.expirience = " how  do you have expirience";
+	this.mainSkills = "basic skills";
+	this.additionSkills = "other skills";
+	this.aboutMyself = "Tell about yourself, that you think important";
 };
