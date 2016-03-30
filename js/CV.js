38c163369e3219d83de43b13b7f24caa67a52380
Ivan456 <ivan.vodich.l@gmail.com>
2016-03-30 15:33:12 +0300

    edit main.js and cv.js. + improve hide button

--- js/CV.js
+++ js/CV.js
@@ -4,14 +4,14 @@ function CV() {
 	this.generalInformation = "др:	05.05.94 <br>  место проживания:	г. Минск, ул. Октябрьская 10 <br> тел.:	8 029 800 63 28 <br> email:	Ivan.Vodich.l@gmail.com";
 	this.education = "БГУ ФПМИ 4 курс";
 	this.wantedProfession = "Front End Developer <br> JS Developer";
-	this.expirience = " 1.5 года дизайнер БГУ";
+	this.expirience = "1.5 года дизайнер БГУ";
 	this.mainSkills = "JS(Ecma Script 5.0), HTML 5, CSS 3, <br> JQuery, Angular, Adobe Photoshop, Adope Illustrator";
 	this.additionalSkills = "Java SE, SQL, C++";
 	this.languages = "English Intermediate"; 
 	this.aboutMyself = "желание постоянно развиваться в своей сфере деятельности, принимать участие в сложных проектах, работа в большой команде";
 };
 
-CV.prototype.addFreeBlock = function (firstName, secondName) {
+CV.prototype.addInformationBlock = function (firstName, secondName) {
 	var newBlock = document.createElement("tr");
 	newBlock.id = firstName;
 	newBlock.className = "table__information-block"
@@ -31,15 +31,6 @@ CV.prototype.addTitleLine = function (firstName, secondName) {
 	place.appendChild(newBlock);
 };
 
-CV.prototype.addStructuredBlock = function () {
-	var newBlock = document.createElement("tr");
-
-	newBlock.innerHTML += "<th>" + "<p>Ivan Vodich</p>" + "</th>"
-	newBlock.innerHTML += "<th>" + "<p>PHOTO</p>" + "</th>";
-	var place = document.getElementById("article");
-	place.appendChild();
-};
-
 CV.prototype.savePDF = function(){
 	var objForHtml2canvas = {
 		onrendered: function (canvas) {
@@ -51,3 +42,62 @@ CV.prototype.savePDF = function(){
 	};
 	html2canvas(document.getElementById("CV"), objForHtml2canvas);
 };
+
+CV.prototype.changeColor = function(){
+	var r, g, b;
+	r = Math.round(255*Math.random());
+	g = Math.round(255*Math.random());
+	b = Math.round(255*Math.random());
+	document.getElementById("CV").style.background = "rgb("+r+","+g+","+b+")";
+};
+	
+CV.prototype.editContent = function(){
+	var thMassive = document.getElementsByTagName("th");
+	for (var i = 0; i < thMassive.length; i++) {
+		thMassive[i].contentEditable = true;
+		thMassive[i].style.border = "2px solid #0000FF";
+	};
+};
+
+CV.prototype.applyContent = function(){
+	var thMassive = document.getElementsByTagName("th");
+	for (var i = 0; i < thMassive.length; i++) {
+		thMassive[i].contentEditable = false;
+		thMassive[i].style.border = "0px none #0000FF";
+	};
+};
+
+CV.prototype.addField = function(){
+	CV.prototype.addTitleLine(); 
+	CV.prototype.addFreeBlock();
+};
+
+CV.prototype.deleteField = function(){
+	var tdMassive = document.getElementsByTagName("tr");
+	var massiveLength = tdMassive.length;
+	document.getElementById("CV").removeChild(tdMassive[massiveLength - 1]);
+	document.getElementById("CV").removeChild(tdMassive[massiveLength - 2]);
+};
+
+CV.prototype.addButton = function(text, funcOnClick){
+	var button = document.createElement("input");
+	var place = document.getElementById("menu");
+	button.value = text;
+	button.type = "button";
+	button.onclick = funcOnClick;
+	place.appendChild(button);
+};
+
+
+CV.prototype.showMenu = function(){
+	document.getElementById("menu").style.display = "block";
+	document.getElementById("CV").style.marginTop = "70px";
+	document.getElementById("menu").onclick = null;
+};
+
+CV.prototype.hideMenu = function(){
+	document.getElementById("menu").style.display = "none";
+	document.getElementById("CV").style.marginTop = "20px";
+	document.getElementById("CV").onclick = CV.prototype.showMenu;
+};
+
--- js/main.js
+++ js/main.js
@@ -1,70 +1,20 @@
 (function () {
-	function addButton (id, text, funcOnClick) {
-		var button = document.createElement("input");
-		var place = document.getElementById("menu");
-		button.value = text;
-		button.type = "button";
-		button.onclick = funcOnClick;
-		place.appendChild(button);
-	};
-
-	function changeColor() {
-		var r, g, b;
-		r = Math.round(255*Math.random());
-		g = Math.round(255*Math.random());
-		b = Math.round(255*Math.random());
-		document.getElementById("CV").style.background = "rgb("+r+","+g+","+b+")";
-	};
-
-	function hideMenu() {
-		document.getElementById("menu").style.display = "none";
-		document.getElementById("CV").style.marginTop = "20px";
-	};
-
-	function editContent() {
-		var thMassive = document.getElementsByTagName("th");
-		for (var i = 0; i < thMassive.length; i++) {
-			thMassive[i].contentEditable = true;
-			thMassive[i].style.border = "2px solid #0000FF";
-		};
-	};
-
-	function applyContent() {
-		var thMassive = document.getElementsByTagName("th");
-		for (var i = 0; i < thMassive.length; i++) {
-			thMassive[i].contentEditable = false;
-			thMassive[i].style.border = "0px none #0000FF";
-		};
-	};
-
-	function addField() {
-		CV.prototype.addTitleLine(); 
-		CV.prototype.addFreeBlock();
-	};
-
-	function deleteField() {
-		var tdMassive = document.getElementsByTagName("tr");
-		var massiveLength = tdMassive.length;
-		document.getElementById("CV").removeChild(tdMassive[massiveLength - 1]);
-		document.getElementById("CV").removeChild(tdMassive[massiveLength - 2]);
-	};
-	
 	var newPerson = new CV();
-	addButton("menu","save as .PDF", newPerson.savePDF);
-	addButton("menu","change color", changeColor);
-	addButton("menu","hide menu", hideMenu);
-	addButton("menu","edit CV", editContent);
-	addButton("menu","apply CV", applyContent);
-	addButton("menu","add field", addField);
-	addButton("menu","delete field", deleteField);
-	newPerson.addFreeBlock("name","photo");
+	newPerson.addButton("save as .PDF", newPerson.savePDF);
+	newPerson.addButton("change color", newPerson.changeColor);
+	newPerson.addButton("hide menu", newPerson.hideMenu);
+	newPerson.addButton("edit CV", newPerson.editContent);
+	newPerson.addButton("apply CV", newPerson.applyContent);
+	newPerson.addButton("add field", newPerson.addField);
+	newPerson.addButton("delete field", newPerson.deleteField);
+	newPerson.addInformationBlock("name","photo");
 	newPerson.addTitleLine("general information","education");
-	newPerson.addFreeBlock("generalInformation","education");
+	newPerson.addInformationBlock("generalInformation","education");
 	newPerson.addTitleLine("wanted profession","expirience");
-	newPerson.addFreeBlock("wantedProfession","expirience");
+	newPerson.addInformationBlock("wantedProfession","expirience");
 	newPerson.addTitleLine("main skills","additional skills");
-	newPerson.addFreeBlock("mainSkills","additionalSkills");
+	newPerson.addInformationBlock("mainSkills","additionalSkills");
 	newPerson.addTitleLine("languages","about myself");
-	newPerson.addFreeBlock("languages","aboutMyself");
+	newPerson.addInformationBlock("languages","aboutMyself");
 	newPerson.toPDF();
 })();
