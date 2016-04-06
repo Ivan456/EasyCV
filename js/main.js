c957fa31f5873bd8c7160ee84552ae9ad133f733
Ivan456 <ivan.vodich.l@gmail.com>
2016-04-06 20:35:04 +0300

    save user data. start

--- js/main.js
+++ js/main.js
@@ -12,6 +12,28 @@ function myFunction() {
     };
 };
 
+var xmlhttp = new XMLHttpRequest();
+var url = "myTutorials.txt";
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        myFunction(myArr);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<a href="' + arr[i].url + '">' + 
+        arr[i].display + '</a><br>';
+    }
+    document.getElementById("id01").innerHTML = out;
+}
+
 	var newPerson = new CV();
 	newPerson.addButton("save as .PDF", newPerson.savePDF);
 	newPerson.addButton("change color", newPerson.changeColor);
