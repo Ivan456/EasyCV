48e88349ecf1c4581aff19b3ddb5a21a184ee185
Ivan456 <ivan.vodich.l@gmail.com>
2016-05-04 17:44:51 +0300

    commit project 3

--- server.js
+++ server.js
@@ -119,15 +119,15 @@ var SampleApp = function() {
      *  Initialize the server (express) and create the routes and register
      *  the handlers.
      */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
+    // self.initializeServer = function() {
+    //     self.createRoutes();
+    //     self.app = express.createServer();
 
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
+    //     //  Add handlers for the app (from the routes).
+    //     for (var r in self.routes) {
+    //         self.app.get(r, self.routes[r]);
+    //     }
+    // };
 
 
     /**
@@ -139,7 +139,7 @@ var SampleApp = function() {
         self.setupTerminationHandlers();
 
         // Create the express server and routes.
-        self.initializeServer();
+        // self.initializeServer();
     };
 
 
@@ -149,22 +149,268 @@ var SampleApp = function() {
 
 
     self.start = function() {
+        http.createServer(function(req, res) {
+	var filename = req.url;
+	if (req.url == "/") {
+		console.log('.....change req.url to "/index.html"');
+		filename = "/index.html";
+	};
+
+	
+	switch(req.url) {
+		case "/registration":
+			var body = '';
+			
+			req.on('data', function(chunk) {
+	    		body += chunk.toString('utf8');
+			});
+			req.on('end', function() {
+	  			body = JSON.parse(body);	
+
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				
+				var con = mysql.createConnection({
+					host: "127.0.0.1",
+					port: "3306",
+					user: "root",
+					password: "1010011010",
+					database: "vodichdb"
+				});
+
+				con.connect(function(err){
+					if(err){
+						console.log('Error connecting to Db' + err);
+						return;
+					};
+					console.log('Connection established');
+				});
+				
+
+				con.query('SELECT password FROM vodichCV WHERE email = "' +
+					body.email + '"', 
+					function(err, rows){
+						if(err){
+							console.log('Error connecting to Db' + err);
+							return;
+						};
+
+						if(rows.length > 0) {
+							res.statusCode = 200;
+							res.end("the email regestered");
+						} else {
+							con.query('INSERT INTO vodichCV (email, password) VALUES ("' + 
+								body.email + '",' + '"' +
+								body.password + '")', 
+								
+								function selectCb(err, results, fields) {
+							    	if (err) console.log('Error insert' + err);
+							    	res.statusCode = 200;
+									res.end("success registration");
+								}
+							);
+						};
+						con.end(function(err) {});
+					}			
+				);
+			});
+			break;
+
+		case "/login":
+			var body = '';
+			
+			req.on('data', function(chunk) {
+	    		body += chunk.toString('utf8');
+			});
+			req.on('end', function() {
+	  			body = JSON.parse(body);	
+
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				
+				var con = mysql.createConnection({
+					host: "127.0.0.1",
+					port: "3306",
+					user: "root",
+					password: "1010011010",
+					database: "vodichdb"
+				});
+
+				con.connect(function(err){
+					if(err){
+						console.log('Error connecting to Db' + err);
+						return;
+					};
+					console.log('Connection established');
+				});
+				
+
+				con.query('SELECT password FROM vodichCV WHERE email = "' +
+					body.email + '"', 
+					function(err, rows){
+						if(err){
+							console.log('Error connecting to Db' + err);
+							return;
+						};
+
+						if(rows.length > 0) {
+							if(confirmPassword(body.password, rows[0].password)) {
+								res.statusCode = 200;
+								res.end("success login");
+							} else {
+								res.statusCode = 200;
+								res.end("password do not confirm");
+							};
+						} else {
+							res.statusCode = 200;
+							res.end("login there is not such email");
+						};
+					con.end(function(err) {});
+					}
+				);
+			});
+			break;
+
+		case "/saveData":
+			var body = '';
+			
+			req.on('data', function(chunk) {
+	    		body += chunk.toString('utf8');
+			});
+			req.on('end', function() {
+	  			body = JSON.parse(body);	
+
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				
+				var con = mysql.createConnection({
+					host: "127.0.0.1",
+					port: "3306",
+					user: "root",
+					password: "1010011010",
+					database: "vodichdb"
+				});
+
+				con.connect(function(err){
+					if(err){
+						console.log('Error connecting to Db' + err);
+						return;
+					};
+					console.log('Connection established' +  body.photo);
+				});
+
+				body.photo = body.photo.replace(/\"/g,"");				
+				con.query('UPDATE vodichCV SET ' +
+						'name = "' + body.name +
+						'", photo = "' + body.photo +
+						'", generalInformation = "' + body.generalInformation +
+						'", education = "' + body.education +
+						'", wantedProfession = "' + body.wantedProfession +
+						'", expirience = "' + body.expirience +
+						'", mainSkills = "' + body.mainSkills +
+						'", additionalSkills = "' + body.additionalSkills +
+						'", languages = "' + body.languages +
+						'", aboutMyself = "' + body.aboutMyself +
+ 						'" WHERE email = "' + body.email + '"',
+
+					function selectCb(err, results, fields) {
+				    	if (err) console.log('Error insert' + err);
+				    	res.statusCode = 200;
+						res.end("success save data");
+					}
+				);
+			});
+			break;
+
+		case "/loadData":
+			var body = '';
+			
+			req.on('data', function(chunk) {
+	    		body += chunk.toString('utf8');
+			});
+			req.on('end', function() {
+	  			body = JSON.parse(body);	
+
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				
+				var con = mysql.createConnection({
+					host: "127.0.0.1",
+					port: "3306",
+					user: "root",
+					password: "1010011010",
+					database: "vodichdb"
+				});
+
+				con.connect(function(err){
+					if(err){
+						console.log('Error connecting to Db' + err);
+						return;
+					};
+					console.log('Connection established' + body.email);
+				});
+				
+
+				con.query('SELECT * FROM vodichCV WHERE email = "' + 
+						body.email + '"',
+
+					function(err, rows){
+						if(err){
+							console.log('Error connecting to Db' + err);
+							return;
+						};
+
+						if(rows.length > 0) {
+							res.statusCode = 200;
+							res.end(JSON.stringify(rows[0]));
+						} else {
+							res.statusCode = 200;
+							res.end("load data: there is not such email");
+						};
+						con.end(function(err) {});
+					}
+				);
+			});
+			break;
+			
+		default:
+			var ext = path.extname(filename),
+				localPath = __dirname,
+				validExtensions = {
+					".html": "text/html",			
+					".css": "text/css",
+					".json": "application/json",
+					".js": "application/javascript", 
+					".ttf": "font/ttf",
+					".txt": "text/plain",
+					".jpg": "image/jpeg",
+					".gif": "image/gif",
+					".png": "image/png",
+					".ico": "image/ico"
+				},
+				isValidExt = validExtensions[ext];
+
+			if (isValidExt) {
+				localPath += filename;
+				fs.exists(localPath, function(exists) {
+					if(exists) {
+						getFile(localPath, res, validExtensions[ext]);
+					} else {
+						console.log("File not found: " + localPath);
+						res.writeHead(404);
+						res.end();
+					};
+				});
+			} else {
+				console.log("Invalid file extension detected: " + ext);
+			};
+		
+	};
+}).listen(self.port, self.ipaddress);
+
         //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
+        // self.app.listen(self.port, self.ipaddress, function() {
+        //     console.log('%s: Node server started on %s:%d ...',
+        //                 Date(Date.now() ), self.ipaddress, self.port);
+        // });
     };
 };   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
 
 
 function getFile(localPath, res, mimeType) {
@@ -188,3 +434,10 @@ function confirmPassword(formPassword, dbPassword) {
 		return false;
 	};
 };
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
